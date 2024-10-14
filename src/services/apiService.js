c6c3d518f1789541ffc0599d6fcf277b764227f1
filetopenguin <arielweiser17@gmail.com>
2024-10-15 00:12:45 +0300

    made cats fatter

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 const apiKey = process.env.REACT_APP_API_KEY;
-const searchTerm = 'Bibi Netanyahu';
-const GIPHY_URL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=&${encodeURIComponent(searchTerm)}&limit=12`;
+const searchTerm = "fat cat";
+const GIPHY_URL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(searchTerm)}&limit=12`;
 
 
 console.log("API Key:", process.env.REACT_APP_API_KEY);
